2e3a36501b3b2d34e9ca3df3dd87924762986d14
Roy Wei <roywjy@gmail.com>
2016-01-29 12:18:02 +0000

    Create integration test and category new/index pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,32 @@
 class CategoriesController < ApplicationController
 
   def index
-    
+    @categories = Category.all
   end
-  
+
   def new
-  
+    @category = Category.new
   end
-  
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Yes, it works!"
+      redirect_to categories_path
+    else
+      render 'categories/new'
+    end
+  end
+
   def show
-    
+
   end
-  
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
 
 
